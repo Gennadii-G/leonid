b74213868377c662b75b9d12361d5dbada6ebca5
Gennadii_Borodin <Gennadii_Borodin@epam.com>
2018-05-11 15:34:13 +0400

    Initial commit

---
+++ src/main/java/com/spacefox/frida/FridaApplication.java
@@ -0,0 +1,22 @@
+package com.spacefox.frida;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridaApplication.class, args);
+	}
+
+}
+
+class FridaCommandLineRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
